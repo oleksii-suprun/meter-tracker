42ee30043ee4aaa9e394c8cd36a167b292b9c12f
Aleksey Suprun <aleksey.suprun@gmail.com>
2016-04-17 19:54:22 +0300

    Code quality improvements

--- meter-tracker-web/src/main/java/com/asuprun/metertracker/web/resource/impl/ImageResourceImpl.java
+++ meter-tracker-web/src/main/java/com/asuprun/metertracker/web/resource/impl/ImageResourceImpl.java
@@ -1,6 +1,5 @@
 package com.asuprun.metertracker.web.resource.impl;
 
-import com.asuprun.metertracker.web.domain.Resource;
 import com.asuprun.metertracker.web.repository.ResourceRepository;
 import com.asuprun.metertracker.web.resource.ImageResource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +15,12 @@ public class ImageResourceImpl implements ImageResource {
 
     @Override
     public Response getImage(String id) {
-        Resource image = resourceRepository.findOne(Long.valueOf(id));
-        if (image == null) {
-            return Response.status(Response.Status.NOT_FOUND).build();
-        }
-        Response.ResponseBuilder response = Response.ok(image.getData());
-        response.header("Content-Disposition", "inline; filename=" + id + ".jpeg");
-        return response.build();
+        return resourceRepository.findById(Long.valueOf(id))
+                .map(i -> {
+                    Response.ResponseBuilder response = Response.ok(i.getData());
+                    response.header("Content-Disposition", "inline; filename=" + id + ".jpeg");
+                    return response.build();
+                })
+                .orElse(Response.status(Response.Status.NOT_FOUND).build());
     }
 }
