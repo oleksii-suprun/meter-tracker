dbeccb265fc287ef7b1bd8f46d06b36c85110184
Aleksey Suprun <aleksey.suprun@gmail.com>
2016-04-18 00:33:32 +0300

    [#7] Code quality improvements (#10)

--- meter-tracker-web/src/main/java/com/asuprun/metertracker/web/repository/ResourceRepository.java
+++ meter-tracker-web/src/main/java/com/asuprun/metertracker/web/repository/ResourceRepository.java
@@ -4,6 +4,10 @@ import com.asuprun.metertracker.web.domain.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
+
+    Optional<Resource> findById(long id);
 }
